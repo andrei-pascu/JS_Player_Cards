b2fa9fad2421521aeba0e7f64466fef8d9ded8fb
Cristian Pascu <30505787+andrei-pascu@users.noreply.github.com>
2020-04-03 14:18:14 +0100

    _

--- js/script.js
+++ js/script.js
@@ -7,21 +7,36 @@ console.log(players[0]['player']['id'])
 
 function buildCardHTML() {
 
-    var target = document.getElementById('card_content_container');
-    target.innerHTML = `
-    <div>
-    <div>
-        <img src="../p${players[0]['player']['id']}.png"/>
-    </div>
+    var DOM_card_container = document.getElementById('card_content_container');
+        var DOM_player_image = DOM_card_container.querySelector('.player_image')
+        var DOM_player_team_logo = DOM_card_container.querySelector('.player_team_logo')
 
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    DOM_player_image.setAttribute("src", `dist/assets/assets/p${players[0]['player']['id']}.png`);
+    // DOM_player_team_logo.setAttribute("src", `assets/assets/p${players[0]['player']['id']}.png`);
     
-    </div>
-    `;
+    // target.innerHTML = `
+    // <div>
+    //     <div class="player_image_container">
+    //         <img 
+    //             class="player_image" 
+    //             src="assets/assets/p${players[0]['player']['id']}.png"
+    //         />
+    //     </div>
+
+    //     <div class="player_details_container>
+    //         <div class="player_team_logo_container">
+    //         </div>
+    //         <h2 class="player_name"></h2>
+    //         <h3 class="player_type"></h3>
+    //         <div class="player_stats_container">
+    //             <div class="player_stats_row">
+    //                 <p class="player_stat_name"></p>
+    //                 <p class="player_stat_value"></p>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+    // `;
 
 }
 

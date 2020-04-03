298e84005d8109cd86434ded328de9c0b4e9ac37
Cristian Pascu <30505787+andrei-pascu@users.noreply.github.com>
2020-04-03 12:17:21 +0100

    Initial setuppp

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// var JSON_data = JSON.parse(the_json)
+
+// alert('on')
+console.log(request_data)
+
+
+// function readTextFile(file, callback) {
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.overrideMimeType("application/json");
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function() {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
+//         }
+//     }
+//     rawFile.send(null);
+// }
+
+// //usage:
+// readTextFile("assets/the_json.json", function(text){
+//     var data = JSON.parse(text);
+//     console.log(data);
+// });

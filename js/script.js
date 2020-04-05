a5f228e357f9592484f5b385c16b5c4a29f4f462
Cristian Pascu <30505787+andrei-pascu@users.noreply.github.com>
2020-04-05 11:09:41 +0100

    Done functionality

--- js/script.js
+++ js/script.js
@@ -8,14 +8,38 @@ var DATA_players;
 var DATA_badges_css;
 var DATA_field_positions;
 var DOM_ROOT;
+var DOM;
 
 (function initData() {
     get_data = request_data;
     DATA_players = get_data['players'];
     DATA_badges_css = team_badges;
     DATA_field_positions = field_positions;
-    DOM_ROOT = document.getElementById('card_content_container');
+    DOM_ROOT = document.getElementById('card_container');
+    DOM = {
+        'player_image': DOM_ROOT.querySelector('.player_image'),
+        'player_team_logo': DOM_ROOT.querySelector('.player_team_logo'),
+        'player_name': DOM_ROOT.querySelector('.player_name'),
+        'player_type': DOM_ROOT.querySelector('.player_type'),
+        'player_stats_container': DOM_ROOT.querySelector('.player_stats_container'),
+        'select_player_list_container': DOM_ROOT.querySelector('#select_player_list_container'),
+        'select_player_input': DOM_ROOT.querySelector('#select_player_input'),
+        'select_player_list_container': DOM_ROOT.querySelector('#select_player_list_container')
+    };
 })();
+
+
+
+function MapPlayerNames(selected_player) {
+    function CREATE_DATA(data, selected_player) {
+        this.player_first_name = data[selected_player]['player']['name']['first'];
+        this.player_last_name = data[selected_player]['player']['name']['last'];
+        this.player_full_name = this.player_first_name + ' ' + this.player_last_name;
+    }
+    var DATA = new CREATE_DATA(DATA_players, selected_player);
+    return DATA['player_full_name'];
+}
+
 
 
 function MapData(selected_player) {
@@ -23,7 +47,7 @@ function MapData(selected_player) {
         var _this = this;
         this.player_first_name = data[selected_player]['player']['name']['first'];
         this.player_last_name = data[selected_player]['player']['name']['last'];
-        this.player_full_name = this.player_first_name + this.player_last_name;
+        this.player_full_name = MapPlayerNames(selected_player);
         this.player_type = field_positions[data[selected_player]['player']['info']['position']];
         this.playerPortrait = "dist/assets/assets/p".concat(data[selected_player]['player']['id'], ".png");
 
@@ -47,26 +71,26 @@ function MapData(selected_player) {
     }
 
     var DATA = new CREATE_DATA(DATA_players, selected_player, DATA_badges_css, DATA_field_positions);
-    console.log(DATA);
     return DATA;
 }
 
 
-function buildCardHTML() {
-    var selected_player = 1;
+function buildCardHTML(selected_player, init) {
     var DATA = MapData(selected_player);
-    var DOM = {
-        'player_image': DOM_ROOT.querySelector('.player_image'),
-        'player_team_logo': DOM_ROOT.querySelector('.player_team_logo'),
-        'player_name': DOM_ROOT.querySelector('.player_name'),
-        'player_type': DOM_ROOT.querySelector('.player_type'),
-        'player_stats_container': DOM_ROOT.querySelector('.player_stats_container')
-    };
+    if (init == 'init') {
+        for (let i = 0; i < DATA_players.length; i++) {
+            console.log(DATA_players[i])
+            console.log(MapPlayerNames(i))
+            DOM['select_player_list_container'].insertAdjacentHTML('beforeend', 
+            '<div key='+i+'>' + MapPlayerNames(i) + '</div>'
+            )
+        }
+    }
     DOM['player_image'].setAttribute('src', DATA.playerPortrait);
     DOM['player_team_logo'].setAttribute('style', DATA.badge_pos);
     DOM['player_name'].innerHTML = DATA.player_full_name;
     DOM['player_type'].innerHTML = DATA.player_type;
-    DOM['player_stats_container'].insertAdjacentHTML('beforeend',
+    DOM['player_stats_container'].innerHTML=
     '<div class="player_stats_row">' +
         '<p class="player_stat_name">Appearances</p>' +
         '<p class="player_stat_value">'+ DATA.stats_display['appearances'] + '</p></div>' +
@@ -81,8 +105,25 @@ function buildCardHTML() {
         '<p class="player_stat_value">'+ DATA.stats_display['goals_per_match'] + '</p></div>' +
     '<div class="player_stats_row">' +
         '<p class="player_stat_name">Passes per minute</p>' +
-        '<p class="player_stat_value">'+ DATA.stats_display['passes_per_min'] + '</p></div>');
+        '<p class="player_stat_value">'+ DATA.stats_display['passes_per_min'] + '</p></div>';
 }
 
 
-document.addEventListener('load', buildCardHTML());
+document.addEventListener('load', 
+    buildCardHTML(0, 'init')
+);
+
+DOM['select_player_input'].addEventListener('click', 
+    function(e) {
+        if (e.target.id === 'select_player_selected') {
+            DOM['select_player_input'].classList.remove('hidden_select')
+        }
+    }
+);
+DOM['select_player_list_container'].addEventListener('click', 
+    function(e) {
+        var target_index = e.target.getAttribute('key');
+        buildCardHTML(target_index)
+        DOM['select_player_input'].classList.add('hidden_select')
+    }
+);
